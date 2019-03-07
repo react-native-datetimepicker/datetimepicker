bd887f86d5595f86ca9cafe7b949b84d8a881926
Martijn Swaagman <info@martijnswaagman.nl>
2019-03-07 12:51:56 +0100

    [test] add more assertions

--- test/index.test.js
+++ test/index.test.js
@@ -2,8 +2,36 @@ import renderer from 'react-test-renderer';
 import DatePicker from '../src/index.js';
 import React from 'react';
 
-test('renders a DatePicker by default', () => {
-  const tree = renderer.create(<DatePicker value={ new Date('08/20/2013') }/>).toJSON();
+describe('DatePicker', function (){
+  it('renders a native Component', () => {
+    const tree = renderer.create(<DatePicker value={ new Date('08/20/2013') }/>).toJSON();
 
-  expect(tree).toMatchSnapshot();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('will pass timestamp to native Component', function () {
+    const date = new Date(156e10);
+    const tree = renderer.create(<DatePicker value={ date }/>).toJSON();
+
+    expect(tree).toHaveProperty(['children', 0, 'props', 'date'], date.getTime());
+  });
+
+  it.todo('calls onDateChange callback');
+  it.todo('calls onChange callback');
+
+  it('has default mode `date`', function () {
+    expect(DatePicker.defaultProps.mode).toEqual('date');
+  });
+
+  it('has reference to picker', function () {
+    expect(new DatePicker()._picker).toBeDefined();
+  });
+
+  it('applies styling to `View` wrapper', function () {
+    const style = { backgroundColor: 'red' };
+    const tree = renderer.create(<DatePicker style={style} value={ new Date('08/20/2013') }/>).toJSON();
+
+    expect(tree).toHaveProperty('props.style.backgroundColor', style.backgroundColor);
+    expect(tree).toMatchSnapshot();
+  });
 });

1500887e737cf91d65255b9c8caa749666b199f1
Martijn Swaagman <info@martijnswaagman.nl>
2019-03-08 11:19:41 +0100

    [fix] remove default state from example

--- example/App.js
+++ example/App.js
@@ -5,7 +5,7 @@ import DatePicker from 'react-native-datetimepicker';
 type Props = {};
 export default class App extends Component<Props> {
   state = {
-    date: new Date('03/08/2020'),
+    date: new Date(),
   }
 
   setDate = date => {

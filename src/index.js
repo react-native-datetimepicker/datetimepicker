90fb9d16cadef187fc6996a8cddb5ba1a1599744
Martijn Swaagman <info@martijnswaagman.nl>
2019-03-08 11:35:46 +0100

    [fix] comment

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  *
- * This is a controlled component version of RCTDatePickerIOS
+ * This is a controlled component version of RNDateTimePicker
  *
  * @format
  * @flow strict-local

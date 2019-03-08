02fb8993cb222a0f86ce35c14497a11769f6e547
Martijn Swaagman <info@martijnswaagman.nl>
2019-03-08 11:40:48 +0100

    [test] assert other modes are passed

--- src/index.js
+++ src/index.js
@@ -93,7 +93,7 @@ type Props = $ReadOnly<{|
 
 export default class DatePicker extends React.Component<Props> {
   static defaultProps = {
-    mode: '',
+    mode: 'date',
   };
 
   _picker: ?React.ElementRef<
--- test/index.test.js
+++ test/index.test.js
@@ -4,7 +4,7 @@ import React from 'react';
 
 describe('DatePicker', function (){
   it('renders a native Component', () => {
-    const tree = renderer.create(<DatePicker value={ new Date('08/20/2013') }/>).toJSON();
+    const tree = renderer.create(<DatePicker value={ new Date('08/20/2013') } />).toJSON();
 
     expect(tree).toMatchSnapshot();
   });
@@ -23,13 +23,25 @@ describe('DatePicker', function (){
     expect(DatePicker.defaultProps.mode).toEqual('date');
   });
 
+  it('renders with mode `time`', function () {
+    const tree = renderer.create(<DatePicker value={ new Date('08/20/2013') } mode="time" />).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders with mode `datetime` (iOS only)', function () {
+    const tree = renderer.create(<DatePicker value={ new Date('08/20/2013') } mode="datetime" />).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
   it('has reference to picker', function () {
     expect(new DatePicker()._picker).toBeDefined();
   });
 
   it('applies styling to `View` wrapper', function () {
     const style = { backgroundColor: 'red' };
-    const tree = renderer.create(<DatePicker style={style} value={ new Date('08/20/2013') }/>).toJSON();
+    const tree = renderer.create(<DatePicker style={style} value={ new Date('08/20/2013') } />).toJSON();
 
     expect(tree).toHaveProperty('props.style.backgroundColor', style.backgroundColor);
     expect(tree).toMatchSnapshot();

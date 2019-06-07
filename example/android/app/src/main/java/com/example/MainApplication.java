94165ea078d0c10e8e85eec84d7a83fd97ede625
dsanudovacas <dsanudovacas@godaddy.com>
2019-06-07 12:59:28 +0100

    Small code format changes

--- example/android/app/src/main/java/com/example/MainApplication.java
+++ example/android/app/src/main/java/com/example/MainApplication.java
@@ -6,7 +6,6 @@ import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
-
 import com.facebook.soloader.SoLoader;
 
 import java.util.Arrays;
@@ -25,8 +24,8 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-          new RNDateTimePickerPackage()
+        new MainReactPackage(),
+        new RNDateTimePickerPackage()
       );
     }
 

13d1bf9a5cc27a662305872b7f06ee1f88634c01
mswaagman <mswaagman@godaddy.com>
2019-07-30 11:31:17 +0200

    [dist] update most dependencies

--- example/App.js
+++ example/App.js
@@ -1,7 +1,7 @@
-import React, {Component} from 'react';
 import {StyleSheet, View, Text, Button, Platform} from 'react-native';
-import moment from 'moment';
 import DateTimePicker from '@react-native-community/datetimepicker';
+import React, {Component} from 'react';
+import moment from 'moment';
 
 type Props = {};
 export default class App extends Component<Props> {

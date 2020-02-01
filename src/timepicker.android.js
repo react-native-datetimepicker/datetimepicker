ac9d79dcec388504774f0a58c7c0d467aa0ec63f
Pavel Balint <pavel.balint@anaplan.com>
2020-02-01 22:10:39 +0000

    Revert timepicker.android.js

--- src/timepicker.android.js
+++ src/timepicker.android.js
@@ -7,15 +7,11 @@
  * @format
  * @flow strict-local
  */
-import {
-  DISPLAY_DEFAULT,
-  TIME_SET_ACTION,
-  DISMISS_ACTION,
-} from './constants';
-import { NativeModules } from 'react-native';
-import { toMilliseconds } from './utils';
+import {DISPLAY_DEFAULT, TIME_SET_ACTION, DISMISS_ACTION} from './constants';
+import {NativeModules} from 'react-native';
+import {toMilliseconds} from './utils';
 
-import type { TimePickerOptions, DateTimePickerResult } from './types';
+import type {TimePickerOptions, DateTimePickerResult} from './types';
 
 export default class TimePickerAndroid {
   /**
@@ -47,8 +43,8 @@ export default class TimePickerAndroid {
    * A time has been selected.
    */
   static +timeSetAction: 'timeSetAction' = TIME_SET_ACTION;
-/**
- * The dialog has been dismissed.
- */
-static + dismissedAction: 'dismissedAction' = DISMISS_ACTION;
+  /**
+   * The dialog has been dismissed.
+   */
+  static +dismissedAction: 'dismissedAction' = DISMISS_ACTION;
 }

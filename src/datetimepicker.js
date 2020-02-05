efb59b129a58cfce6a3ae6c46e945d350a51616a
Aike van den Brink <aikevdbrink@live.nl>
2020-02-05 09:51:13 +0100

    Update src/datetimepicker.js
    Co-Authored-By: Evan Bacon <baconbrix@gmail.com>

--- src/datetimepicker.js
+++ src/datetimepicker.js
@@ -1,1 +1,6 @@
-export default undefined;
+export default function DateTimePicker() {
+  React.useEffect(() => {
+    console.warn(`DateTimePicker is not supported on: ${Platform.OS}`);
+  }, [])
+  return null;
+}

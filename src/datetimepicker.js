650c2e0478d418a9132bdd4b8d4445d8abfe13fb
Aike van den Brink <aikevandenbrink@gmail.com>
2020-02-05 09:55:37 +0100

    Resolve problems by importing the correct packages.

--- src/datetimepicker.js
+++ src/datetimepicker.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import { Platform } from 'react-native';
+
 export default function DateTimePicker() {
   React.useEffect(() => {
     console.warn(`DateTimePicker is not supported on: ${Platform.OS}`);

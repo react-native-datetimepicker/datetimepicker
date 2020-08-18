7e984a229d4a7c319811db675a1b1547b6281e25
Vojtech Novak <vonovak@gmail.com>
2020-08-18 16:50:28 +0200

    chore: refactor e2e tests (#251)
    * chore: improve e2e tests
    
    * fix placeholder color
    
    * use isIOS()
    
    * minor updates to test labels

--- example/e2e/utils/matchers.js
+++ example/e2e/utils/matchers.js
@@ -1,7 +1,11 @@
 const getTimeText = () => element(by.id('timeText'));
 const getDateText = () => element(by.id('dateText'));
+const elementById = (id) => element(by.id(id));
+const elementByText = (text) => element(by.text(text));
 
 module.exports = {
   getTimeText,
   getDateText,
+  elementById,
+  elementByText,
 };

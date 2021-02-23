827e47f5cc8f741b203b3d921960666275ed1643
Vojtech Novak <vonovak@gmail.com>
2021-02-23 14:58:59 +0100

    chore: increase timeouts

--- example/App.js
+++ example/App.js
@@ -193,7 +193,7 @@ export const App = () => {
                   setShow(true);
                   setTimeout(() => {
                     setShow(false);
-                  }, 5000);
+                  }, 6000);
                 }}
                 title="Show and dismiss picker!"
               />

5f0c8ee98b13775e7fbb38b902b84d32774b8790
Vojtech Novak <vonovak@gmail.com>
2021-04-15 23:45:17 +0200

    chore: refer to android styles directly by ID (#435)

--- android/src/main/java/com/reactcommunity/rndatetimepicker/RNTimePickerDialogFragment.java
+++ android/src/main/java/com/reactcommunity/rndatetimepicker/RNTimePickerDialogFragment.java
@@ -75,16 +75,12 @@ public class RNTimePickerDialogFragment extends DialogFragment {
       switch (display) {
         case CLOCK:
         case SPINNER:
-          String resourceName = display == RNTimePickerDisplay.CLOCK
-                  ? "ClockTimePickerDialog"
-                  : "SpinnerTimePickerDialog";
+          int theme = display == RNTimePickerDisplay.CLOCK
+                  ? R.style.ClockTimePickerDialog
+                  : R.style.SpinnerTimePickerDialog;
           return new RNDismissableTimePickerDialog(
                   activityContext,
-                  activityContext.getResources().getIdentifier(
-                          resourceName,
-                          "style",
-                          activityContext.getPackageName()
-                  ),
+                  theme,
                   onTimeSetListener,
                   hour,
                   minute,

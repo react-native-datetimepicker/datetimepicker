cc670c334f286bb66a1c1676520d2fd0e6db302b
manojmalik20 <manoj@brightscout.com>
2021-06-02 13:39:54 +0530

    modified timezoneOffsetInMinutes prop and corresponding e2e test cases

--- example/e2e/detoxTest.spec.js
+++ example/e2e/detoxTest.spec.js
@@ -185,7 +185,7 @@ describe('Example', () => {
       await userChangesMinuteValue();
       await userTapsOkButtonAndroid();
 
-      await expect(timeText).toHaveText('22:30');
+      await expect(timeText).toHaveText('23:30');
     }
   });
 

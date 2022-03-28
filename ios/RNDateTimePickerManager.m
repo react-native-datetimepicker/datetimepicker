1181f790651c5eb44b3140ed4267452f68bde5bf
Geraint White <geraintwhite@gmail.com>
2022-03-28 09:17:20 +0100

    UIColor tintColor in Xcode 12

--- ios/RNDateTimePickerManager.m
+++ ios/RNDateTimePickerManager.m
@@ -12,6 +12,12 @@
 #import "RNDateTimePicker.h"
 #import <React/UIView+React.h>
 
+#if __IPHONE_OS_VERSION_MAX_ALLOWED < 15000
+@interface UIColor (Xcode12)
++ (instancetype) tintColor;
+@end
+#endif
+
 @implementation RCTConvert(UIDatePicker)
 
 RCT_ENUM_CONVERTER(UIDatePickerMode, (@{

24f345239a147f2a4d2313098aa04a22bd27e4b6
Chiara Mooney <34109996+chiaramooney@users.noreply.github.com>
2022-04-28 21:40:14 -0700

    fix: Break in Windows JS following #574 (#605)
    * Fix Break in Windows
    
    * Fix Format
    
    * Fix Format
    
    * Add Type Info

---
+++ src/picker.windows.js
@@ -0,0 +1,15 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @format
+ * @flow strict-local
+ */
+import {requireNativeComponent} from 'react-native';
+
+export default (requireNativeComponent(
+  'RNDateTimePickerWindows',
+  // $FlowFixMe Flow: Unclear type. Using `any`, `Object`, or `Function` types is not safe!
+): any);

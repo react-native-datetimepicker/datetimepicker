7d20f1f7284ed282cba6b6e73140994797db7c68
Vojtech Novak <vonovak@gmail.com>
2022-06-27 23:44:27 +0200

    add forgotten file

---
+++ src/DateTimePickerAndroid.js
@@ -0,0 +1,7 @@
+import {Platform} from 'react-native';
+
+const warn = () => {
+  console.warn(`DateTimePickerAndroid is not supported on: ${Platform.OS}`);
+};
+
+export const DateTimePickerAndroid = {open: warn, dismiss: warn};

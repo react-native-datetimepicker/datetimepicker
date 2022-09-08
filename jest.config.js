733c2244525541a4d53d6b7ab0a9f04908b4b736
Alfonso Curbelo <alfonso.curbelo@coinbase.com>
2022-09-08 10:51:04 -0400

    Upgrade example to 0.70

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,6 @@
 module.exports = {
   preset: 'react-native',
   modulePathIgnorePatterns: ['<rootDir>/example/node_modules'],
-  transform: {
-    '^.+\\.js$': require.resolve('react-native/jest/preprocessor.js'),
-  },
   setupFiles: ['./jest/setup.js'],
   testRegex: '(/__tests__/.*|(\\.|/)(test))\\.[jt]sx?$',
 };

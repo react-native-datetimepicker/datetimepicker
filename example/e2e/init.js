7c706f115f653f9103fe09935d1ede87d310037c
Vojtech Novak <vonovak@gmail.com>
2023-06-20 11:51:36 +0200

    chore: android emulator changes

--- example/e2e/init.js
+++ example/e2e/init.js
@@ -1,6 +1,3 @@
-// Set the default test timeout
-jest.setTimeout(130000);
-
 beforeAll(async () => {
   await device.launchApp();
 }, 300000);
---
+++ example/e2e/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  setupFilesAfterEnv: ['./init.js'],
+  maxWorkers: 1,
+  testTimeout: 120000,
+  verbose: true,
+  reporters: ['detox/runners/jest/reporter'],
+  globalSetup: 'detox/runners/jest/globalSetup',
+  globalTeardown: 'detox/runners/jest/globalTeardown',
+  testEnvironment: 'detox/runners/jest/testEnvironment',
+};

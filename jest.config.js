e0c64986b9154b4258a0b02413476388a6ad033a
David Yarzebinski <dyarzebinski97@gmail.com>
2023-07-09 17:52:05 -0400

    update jest.config.js for builder bob

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   preset: 'react-native',
-  modulePathIgnorePatterns: ['<rootDir>/example/node_modules'],
+  modulePathIgnorePatterns: [
+    '<rootDir>/example/node_modules',
+    '<rootDir>/lib/',
+  ],
   setupFiles: ['./jest/setup.js'],
   testRegex: '(/__tests__/.*|(\\.|/)(test))\\.[jt]sx?$',
 };

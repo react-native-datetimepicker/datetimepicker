21441e3fc8b2e5a59ab71e477d97e6ac67efb557
Shawn Dempsey <shawndempsey@meta.com>
2024-04-02 19:45:03 -0700

    Revert babel-config.js

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,11 @@ const isLintingOrTesting =
   process.env.NODE_ENV === 'test' || process.env.NODE_ENV === 'lint';
 
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: [
+    [
+      'module:metro-react-native-babel-preset',
+      // this is a workaround for some deeper issue
+      {useTransformReactJSXExperimental: !isLintingOrTesting},
+    ],
+  ],
 };

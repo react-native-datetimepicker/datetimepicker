67a737e6704b11e8d4a7774572d918ad41f9344f
Vojtech Novak <vonovak@gmail.com>
2024-04-25 19:50:03 +0200

    feat: remove jest/setup.js (#793)
    BREAKING CHANGE: jest/setup.js was removed as it's not needed

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   preset: 'react-native',
   modulePathIgnorePatterns: ['<rootDir>/example/node_modules'],
-  setupFiles: ['./jest/setup.js'],
   testRegex: '(/__tests__/.*|(\\.|/)(test))\\.[jt]sx?$',
 };

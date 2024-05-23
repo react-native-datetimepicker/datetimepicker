bd176afdda84144634ddae9cdbc481822333199b
lavi02 <hoil.jeong@fleetune.com>
2024-05-23 14:15:13 +0900

    [ADD] oldArch

---
+++ android/src/oldArch/com/reactcommunity/rndatetimepicker/NativeModuleTimePickerSpec.java
@@ -0,0 +1,22 @@
+package com.reactcommunity.rndatetimepicker;
+
+import com.facebook.proguard.annotations.DoNotStrip;
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.ReactModuleWithSpec;
+
+public abstract class NativeModuleTimePickerSpec extends ReactContextBaseJavaModule implements ReactModuleWithSpec {
+  public NativeModuleTimePickerSpec(ReactApplicationContext reactContext) {
+    super(reactContext);
+  }
+
+  @ReactMethod
+  @DoNotStrip
+  public abstract void dismiss(Promise promise);
+
+  @ReactMethod
+  @DoNotStrip
+  public abstract void open(ReadableMap params, Promise promise);
+}

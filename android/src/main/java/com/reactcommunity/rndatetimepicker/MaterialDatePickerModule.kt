a2a48b9da16183b318ecb452eeb02586cbf96914
sidorchukandrew <36050911+sidorchukandrew@users.noreply.github.com>
2024-12-13 15:07:12 -0500

    Implement MaterialDatePicker
    This also adds a utility function to RNDate to get the date in
    milliseconds.

--- android/src/main/java/com/reactcommunity/rndatetimepicker/MaterialDatePickerModule.kt
+++ android/src/main/java/com/reactcommunity/rndatetimepicker/MaterialDatePickerModule.kt
@@ -1,8 +1,12 @@
 package com.reactcommunity.rndatetimepicker
 
+import androidx.fragment.app.FragmentActivity
 import com.facebook.react.bridge.Promise
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReadableMap
+import com.facebook.react.bridge.UiThreadUtil
+import com.reactcommunity.rndatetimepicker.Common.createDatePickerArguments
+import com.reactcommunity.rndatetimepicker.Common.dismissDialog
 
 class MaterialDatePickerModule(reactContext: ReactApplicationContext): NativeModuleMaterialDatePickerSpec(reactContext) {
   companion object {
@@ -14,8 +18,27 @@ class MaterialDatePickerModule(reactContext: ReactApplicationContext): NativeMod
   }
 
   override fun dismiss(promise: Promise?) {
+    val activity = currentActivity as FragmentActivity?
+    dismissDialog(activity, NAME, promise)
   }
 
   override fun open(params: ReadableMap, promise: Promise) {
+    val activity = currentActivity as FragmentActivity?
+    if (activity == null) {
+      promise.reject(
+        RNConstants.ERROR_NO_ACTIVITY,
+        "Tried to open a MaterialDatePicker dialog while not attached to an Activity"
+      )
+      return
+    }
+
+    val fragmentManager = activity.supportFragmentManager
+
+    UiThreadUtil.runOnUiThread {
+      val arguments = createDatePickerArguments(params)
+      val datePicker =
+        RNMaterialDatePicker(arguments, promise, fragmentManager, reactApplicationContext)
+      datePicker.open()
+    }
   }
 }

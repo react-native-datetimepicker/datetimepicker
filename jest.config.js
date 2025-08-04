7c8929ffa924b75d0401d4d350ca02fe52fd3f04
Jordan Phillips <jordypee27@gmail.com>
2025-08-04 13:15:14 +1000

    chore: upgrade to expo v53

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  preset: 'react-native',
+  preset: 'jest-expo',
   modulePathIgnorePatterns: ['<rootDir>/example/node_modules'],
   testRegex: '(/__tests__/.*|(\\.|/)(test))\\.[jt]sx?$',
 };

65c12f0649d283a378c2e5b5c1fcffd4c3dc02d3
Protik Biswas <protikbiswas100@microsoft.com>
2025-11-26 14:15:20 +0530

    adding demo example

---
+++ example/windows/DateTimePickerDemo/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::DateTimePickerDemo::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::DateTimePickerDemo::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
